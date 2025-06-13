ab2aa93dff6c6c3423b586913f8f5f3dedb9a906
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-06-13 00:54:43 -0400

    Se genera navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../service/auth.service';
+
+// Import Bootstrap JS (make sure bootstrap is installed in your project)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// Declare bootstrap for TypeScript
+declare const bootstrap: any;
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  username: string = '';
+  password: string = '';
+  loginError: string = '';
+  userName: string = '';
+  isLoggedIn: boolean = false;
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.getUser().subscribe(user => {
+      this.userName = user?.username || '';
+      this.isLoggedIn = !!user;
+    });
+  }
+
+  login() {
+    const success = this.authService.login(this.username, this.password);
+    if (!success) {
+      this.loginError = 'Usuario o contraseña incorrectos';
+    } else {
+      this.loginError = '';
+      const offcanvasEl = document.getElementById('loginOffcanvas');
+      const bsOffcanvas = bootstrap.Offcanvas.getInstance(offcanvasEl!) || new bootstrap.Offcanvas(offcanvasEl!);
+      bsOffcanvas.hide(); // cerrar manualmente
+    }
+  }
 }
