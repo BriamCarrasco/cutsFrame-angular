af15b92d74cf091c44d3f0459536ea0430ab448f
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-06-14 15:01:07 -0400

    Se programa componente de camara

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RegistroComponent } from './registro/registro.component';
 import { CategoriaComponent } from './categoria/categoria.component';
+import { CamaraDetalleComponent } from './camara-detalle/camara-detalle.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },      
   { path: 'registro', component: RegistroComponent },
-  { path: 'categorias/:categoria', component: CategoriaComponent }
+  { path: 'categorias/:categoria', component: CategoriaComponent },
+  { path: 'camara/:id', component: CamaraDetalleComponent }
 ];
 
 @NgModule({
--- src/app/camara-detalle/camara-detalle.component.ts
+++ src/app/camara-detalle/camara-detalle.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CameraService, Camera } from 'src/service/camara.service';
 
 @Component({
   selector: 'app-camara-detalle',
   templateUrl: './camara-detalle.component.html',
   styleUrls: ['./camara-detalle.component.scss']
 })
-export class CamaraDetalleComponent {
+export class CamaraDetalleComponent implements OnInit {
+  camara?: Camera;
 
+  constructor(
+    private route: ActivatedRoute,
+    private cameraService: CameraService
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id') || '';
+    this.camara = this.cameraService.getCamera(id);
+  }
 }
