1d79977bb3723d8ac60e0110576fc78f0b28489e
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-06-17 01:24:17 -0400

    Se modifica routes para separar admin y public

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,18 +5,35 @@ import { RegistroComponent } from './registro/registro.component';
 import { CategoriaComponent } from './categoria/categoria.component';
 import { CamaraDetalleComponent } from './camara-detalle/camara-detalle.component';
 import { adminGuard } from './guards/admin.guard';
+import { PublicLayoutComponent } from './layouts/public-layout/public-layout.component';
+import { CamarasComponent } from './admin/camaras/camaras.component';
+import { PanelAdminComponent } from './admin/panel-admin/panel-admin.component';
+import { UsuariosComponent } from './admin/usuarios/usuarios.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },      
-  { path: 'registro', component: RegistroComponent },
-  { path: 'categorias/:categoria', component: CategoriaComponent },
-  { path: 'camara/:id', component: CamaraDetalleComponent },
-  { 
-  path: 'panel-admin', 
-  loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
-  canActivate: [adminGuard] 
+  {
+    path: '',
+    component: PublicLayoutComponent,
+    children: [
+      { path: '', component: HomeComponent },
+      { path: 'registro', component: RegistroComponent },
+      { path: 'categorias/:categoria', component: CategoriaComponent },
+      { path: 'camara/:id', component: CamaraDetalleComponent },
+      // ...otras rutas públicas
+    ]
+  },
+  {
+    path: 'panel-admin',
+    component: AdminLayoutComponent,
+    canActivate: [adminGuard], // <-- Aquí aplicas el guard
+    children: [
+      { path: '', component: PanelAdminComponent },
+      { path: 'camaras', component: CamarasComponent },
+      { path: 'usuarios', component: UsuariosComponent },
+      // ...otras rutas admin
+    ]
   }
 ];
 
