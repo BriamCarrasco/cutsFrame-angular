89ff9373319928790778f52ca1a327ad17a4503a
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-06-19 03:36:57 -0400

    Se modifica vista de categoria

--- src/app/admin/categorias/categorias.component.ts
+++ src/app/admin/categorias/categorias.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Categoria, CategoriaService } from 'src/service/categoria.service';
+
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-categorias',
   templateUrl: './categorias.component.html',
   styleUrls: ['./categorias.component.scss']
 })
-export class CategoriasComponent {
+export class CategoriasComponent implements OnInit {
+  categorias: Categoria[] = [];
+  categoriaEditando: Categoria = { id: '', nombre: '', descripcion: '' };
 
+  constructor(private categoriaService: CategoriaService) {}
+
+  ngOnInit(): void {
+    this.cargarCategorias();
+  }
+
+  cargarCategorias() {
+    this.categorias = this.categoriaService.getCategorias();
+  }
+
+  abrirFormularioNuevaCategoria() {
+    this.categoriaEditando = { id: '', nombre: '', descripcion: '' };
+    const modal = new bootstrap.Modal(document.getElementById('categoriaModal'));
+    modal.show();
+  }
+
+  editarCategoria(categoria: Categoria) {
+    this.categoriaEditando = { ...categoria };
+    const modal = new bootstrap.Modal(document.getElementById('categoriaModal'));
+    modal.show();
+  }
+
+  guardarCategoria() {
+    if (this.categoriaEditando.id) {
+      this.categoriaService.actualizarCategoria(this.categoriaEditando);
+    } else {
+      this.categoriaEditando.id = Date.now().toString();
+      this.categoriaService.agregarCategoria(this.categoriaEditando);
+    }
+    this.cargarCategorias();
+    bootstrap.Modal.getInstance(document.getElementById('categoriaModal')).hide();
+  }
+
+  eliminarCategoria(categoria: Categoria) {
+    if (confirm('¿Seguro que deseas eliminar esta categoría?')) {
+      this.categoriaService.eliminarCategoria(categoria.id);
+      this.cargarCategorias();
+    }
+  }
 }
