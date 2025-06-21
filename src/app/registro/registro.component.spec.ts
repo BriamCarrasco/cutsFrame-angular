07aa9cf837d530eb15c00fea82380b9a8365ef02
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-06-20 23:19:21 -0400

    Se generan pruebas unitarias para mensajes de error por usuario ya existente y por si password no coinciden

--- src/app/registro/registro.component.spec.ts
+++ src/app/registro/registro.component.spec.ts
@@ -21,4 +21,29 @@ describe('RegistroComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Mostrar error si las password no coinciden', () => {
+  component.registroForm.patchValue({
+    password: 'Qwerty123!',
+    confirmarPassword: 'Qwerty123.'
+  });
+    expect(component.registroForm.hasError('noCoincide')).toBeTrue();
+  });
+
+  it('Mostrar error si el usuario ya existe.', () => {
+    spyOn(component['toastr'], 'error');
+    localStorage.setItem('usuarios', JSON.stringify([{ usuario: 'usuarioTest' }]));
+    component.registroForm.patchValue({
+      usuario: 'usuarioTest',
+      password: 'Qwerty123!',
+      confirmarPassword: 'Qwerty123!',
+      email: 'test@aa.cl',
+      nombre: 'Test',
+      apellido: 'User',
+      fechaNacimiento: '2000-01-01'
+    });
+    component.onSubmit();
+    expect(component['toastr'].error).toHaveBeenCalledWith('El nombre de usuario ya está registrado', 'Error');
+  });
+  
 });
