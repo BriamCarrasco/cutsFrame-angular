4e50bf1345bd447ec2d69c1e6a8a58b8a6763430
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-06-26 01:35:54 -0400

    Se agregan comentarios JSDoc

--- src/app/layouts/public-layout/public-layout.component.ts
+++ src/app/layouts/public-layout/public-layout.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+/**
+ * Componente de layout público para CutsFrame.
+ * 
+ * Define la estructura visual y de navegación para las vistas públicas de la aplicación,
+ * incluyendo la barra de navegación, el contenido principal y el pie de página.
+ */
 @Component({
   selector: 'app-public-layout',
   templateUrl: './public-layout.component.html',
