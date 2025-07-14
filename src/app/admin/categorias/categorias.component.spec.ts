07c2acc2e4e6b58a2236c38f8023f7c08bdb1351
BriamCarrasco <briamcarrasco.f@gmail.com>
2025-07-14 16:01:40 -0400

    Se agrega import ReactiveFormModule

--- src/app/admin/categorias/categorias.component.spec.ts
+++ src/app/admin/categorias/categorias.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { CategoriasComponent } from './categorias.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('CategoriasComponent', () => {
   let component: CategoriasComponent;
@@ -10,7 +11,7 @@ describe('CategoriasComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [CategoriasComponent],
-      imports: [FormsModule, HttpClientModule]
+      imports: [FormsModule, HttpClientModule,ReactiveFormsModule]
     });
     fixture = TestBed.createComponent(CategoriasComponent);
     component = fixture.componentInstance;
